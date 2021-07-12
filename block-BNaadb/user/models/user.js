5a7b56cae2df72a640514c6679f5e822cdaa0de6
Aditya Pathak <mynewchilly@gmail.com>
2021-07-12 15:09:52 +0530

    Re

---
+++ block-BNaadb/user/models/user.js
@@ -0,0 +1,14 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema(
+  {
+    name: String,
+    age: Number,
+    email: { type: String, required: true, match: /@/ },
+  },
+  { timestamps: true }
+);
+
+let User = mongoose.model("User", userSchema);
+module.exports = User;
