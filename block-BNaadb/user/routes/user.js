b06927ff2524831cb80778716222720f49d8b3d6
Aditya Pathak <mynewchilly@gmail.com>
2021-07-12 15:14:14 +0530

    Re

--- block-BNaadb/user/routes/user.js
+++ block-BNaadb/user/routes/user.js
@@ -2,15 +2,25 @@ let express = require("express");
 let router = express.Router();
 let User = require("../models/user");
 
+//CREATE new USER
 router.get("/new", (req, res) => {
   res.render("userForm");
 });
+router.post("/", (req, res, next) => {
+  User.create(req.body, (error, createdUser) => {
+    if (error) return next(error);
+    else res.redirect("/users");
+  });
+});
+
+//READ all users
 router.get("/", (req, res, next) => {
   User.find({}, (error, allUsers) => {
     if (error) return next(error);
     else res.render("allUser", { users: allUsers });
   });
 });
+//READ single user by ID
 router.get("/:id", (req, res, next) => {
   let id = req.params.id;
   User.findById(id, (error, singleUser) => {
@@ -18,18 +28,12 @@ router.get("/:id", (req, res, next) => {
     else res.render("singleUser", { user: singleUser });
   });
 });
+//UPDATE single user by ID
 router.get("/:id/edit", (req, res, next) => {
   let id = req.params.id;
   User.findById(id, (error, singleUser) => {
     if (error) return next(error);
     else res.render("editForm", { user: singleUser });
-  });
-});
-router.get("/:id/delete", (req, res, next) => {
-  let id = req.params.id;
-  User.findByIdAndDelete(id, (error, singleUser) => {
-    if (error) return next(error);
-    else res.redirect("/users");
   });
 });
 router.post("/:id", (req, res, next) => {
@@ -39,10 +43,14 @@ router.post("/:id", (req, res, next) => {
     res.redirect("/users");
   });
 });
-router.post("/", (req, res, next) => {
-  User.create(req.body, (error, createdUser) => {
+
+//DELETE single User by Id
+router.get("/:id/delete", (req, res, next) => {
+  let id = req.params.id;
+  User.findByIdAndDelete(id, (error, singleUser) => {
     if (error) return next(error);
     else res.redirect("/users");
   });
 });
+
 module.exports = router;
